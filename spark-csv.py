b93c9a957c5a1d814b90d094abb9a803ac3ddf96
Ananth <ab7325@nyu.edu>
2018-03-26 17:29:58 -0400

    modify dates to dd-mm-yyyy

--- spark-csv.py
+++ spark-csv.py
@@ -1,5 +1,12 @@
-sqlContext = SQLContext(sc)
-df = sqlContext.read.format('com.databricks.spark.csv').options(header='false', delimiter='\t', inferschema='true').load('/user/ab7325/data.gdeltproject.org/events/*.CSV')
 from pyspark.sql.functions import desc
+sqlContext = SQLContext(sc)
+
+df = sqlContext.read.format('com.databricks.spark.csv').options(header='false', delimiter='\t', inferschema='true').load('/user/ab7325/data.gdeltproject.org/events/*.CSV')
 cs = df.groupBy('_c7').count()
 cs.sort(desc('count')).show(200)
+
+dis = sqlContext.read.format('com.databricks.spark.csv').options(header='true', delimiter='\t', inferschema='true').load('/user/ab7325/data.gdeltproject.org/gkg/*.csv')
+ds = dis.filter(dis['COUNTTYPE']=='SICKENED').groupBy('GEO_COUNTRYCODE').count()
+ds.sort(desc('count')).show(200)
+
+events = sqlContext.read.format('com.databricks.spark.csv').options(header='true', delimiter=',', inferschema='true').load('/user/ab7325/disease/disease_events.csv')
